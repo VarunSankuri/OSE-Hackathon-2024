f12033ec6ba3bebc40a45ba1001f44e6be3816e0
VarunSankuri <sankurivarun@gmail.com>
2024-12-03 14:59:25 -0600

    Update OSEHackathon.py

--- OSEHackathon.py
+++ OSEHackathon.py
@@ -19,7 +19,7 @@ import chromadb
 
 chromadb.api.client.SharedSystemClient.clear_system_cache()
 
-st.title("Cloud Current")
+st.title("OSE Hackathon")
 st.markdown("""
 <style>
 .big-font {
@@ -27,11 +27,7 @@ st.markdown("""
 }
 </style>
 """, unsafe_allow_html=True)
-st.markdown('<p class="big-font">Developed by Varun Sankuri</p>', unsafe_allow_html=True)
-st.caption("Frustrated with ChatGPT and Google Gemini giving you outdated cloud info? Big box models can't keep up with the Cloud's rapid pace."
-           " CloudCurrent is updated much more frequently and also lets you upload your OWN PDFs to get the most accurate, "
-           "'up-to-the-minute answers'. Try it now!")
-st.caption("For questions contact cloudcurrentapp@gmail.com")
+
 
 # Load environment variables from .env file
 load_dotenv()
