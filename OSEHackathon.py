c58637af1bc3904c1b1025a5b9cd99687744cafc
VarunSankuri <sankurivarun@gmail.com>
2024-12-05 16:08:49 -0600

    Update OSEHackathon.py

--- OSEHackathon.py
+++ OSEHackathon.py
@@ -103,14 +103,14 @@ with tab1:
         # Define Prompt Template
         prompt_template = """
         Question: \n{question}\n
+
+        Context:\n {context}?\n
         
         You are a helpful AI assistant helping people answer their Cloud development and
         deployment questions. Answer the question as detailed as possible from the provided context,
         make sure to provide all the details and code if possible, if the answer is not in
         provided context use your  knowledge or imagine an answer but never say that you don't have an answer
         or can't provide an answer based on current context ",
-
-        Context:\n {context}?\n
       
         Answer:
         """
